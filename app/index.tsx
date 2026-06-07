d1e0bbdbcdb725206c6a37ee99aaf321fba44a02
InesBoc <mariainesbocanera@yahoo.com.ar>
2026-06-06 23:18:20 -0300

    cronograma

--- app/index.tsx
+++ app/index.tsx
@@ -218,10 +218,13 @@ export default function Index() {
         
         <View style={styles.divider} />
         <Text style={styles.cronogramaTitle}>📌 Cronograma General</Text>
-        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>09:00 hs</Text> - Desfile de Delegaciones</Text>
+        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>09:00 hs</Text> - Acreditaciones (Cantina)</Text>
+        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>09:30 hs</Text> - Acto de Inauguración (Cancha 2)</Text>
         <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>10:00 hs</Text> - Inicio de Partidos</Text>
-        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>13:00 hs</Text> - Almuerzo y Tercer Tiempo</Text>
-        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>14:00 hs</Text> - Concurso de Coreos 💃 y Sorteos</Text>
+        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>12:30 hs</Text> - Almuerzo 10 ma</Text>
+        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>13:00 hs</Text> - Almuerzo 9na</Text>
+        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>13:30 hs</Text> - Almuerzo 8va</Text>
+        <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>14:00 hs</Text> - Concurso de Coreos 💃 y Sorteos (Cancha 2)</Text>
         <Text style={styles.infoText}>• <Text style={{ fontWeight: '600' }}>16:00 hs</Text> - Cierre</Text>
       </View>
     
